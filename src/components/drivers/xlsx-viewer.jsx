f15d304baa43162802e7bdd2fc0b21331b1fe293
Chao Wang <chao.wang@kaamel.com>
2023-01-04 13:59:23 +0800

    dev

--- src/components/drivers/xlsx-viewer.jsx
+++ src/components/drivers/xlsx-viewer.jsx
@@ -15,16 +15,59 @@ class XlxsViewer extends Component {
     const dataArr = new Uint8Array(this.props.data);
     const arr = [];
 
+
     for (let i = 0; i !== dataArr.length; i += 1) {
       arr.push(String.fromCharCode(dataArr[i]));
     }
-
-    const workbook = XLSX.read(arr.join(''), { type: 'binary' });
+    const workbook = XLSX.read(arr.join(''), { type: 'binary', cellStyles: true, cellDates: true, cellHTML: true });
     const names = Object.keys(workbook.Sheets);
-    const sheets = names.map(name => (
-      XLSX.utils.sheet_to_csv(workbook.Sheets[name])
-    ));
-
+    const sheets = names.map((name) => {
+      let mc = -1;
+      let mr = -1;
+      if (workbook.Sheets[name]['!merges']) {
+        workbook.Sheets[name]['!merges'].forEach((m) => {
+          if (m.e.c > mc) {
+            mc = m.e.c;
+          }
+          if (m.s.c > mc) {
+            mc = m.s.c;
+          }
+          if (m.e.r > mr) {
+            mr = m.e.r;
+          }
+          if (m.s.r > mr) {
+            mr = m.s.r;
+          }
+        });
+      }
+      console.log(name, mc, mr);
+      let j = XLSX.utils.sheet_to_json(workbook.Sheets[name], { blankRows: true, defval: null, header: 1 });
+      console.log(name, j);
+      if (j) {
+        j.forEach((item, index) => {
+          // const f = item.filter(value => index <= mr || (index > mr && value !== null));
+          const f = item.filter(value => value !== null);
+          if (f.length > 0) {
+            const lastindex = item.lastIndexOf(f[f.length - 1]);
+            if (lastindex > mc) {
+              mc = lastindex;
+            }
+            if (index > mr) {
+              mr = index;
+            }
+          }
+        });
+        if (mr > 10) {
+          j = j.filter((item, index) => index <= mr);
+        }
+        j = j.map(item => item.slice(0, mc + 1));
+        console.log(name, mc, mr);
+        console.log('==', name, j);
+        const ns = XLSX.utils.json_to_sheet(j, { skipHeader: true });
+        return XLSX.utils.sheet_to_html(ns, { defval: null });
+      }
+      return XLSX.utils.sheet_to_html(workbook.Sheets[name], { defval: null });
+    });
     return { sheets, names, curSheetIndex: 0 };
   }
 
@@ -59,7 +102,7 @@ class XlxsViewer extends Component {
     return (
       <div className="spreadsheet-viewer">
         {this.renderSheetNames(names)}
-        {this.renderSheetData(sheets[curSheetIndex || 0])}
+        <div style={{ background: 'white' }} dangerouslySetInnerHTML={{ __html: sheets[curSheetIndex || 0] }} />
       </div>
     );
   }
